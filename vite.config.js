29d77ee3475c33a50a053a54464db9baee8812c3
BhimaRakesh <brakeshrakesh5@gmail.com>
2025-01-28 03:44:22 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/RR-MART/'  // Update this to match your GitHub repository name
+  plugins: [react()]
+  // Update this to match your GitHub repository name
 });
 
