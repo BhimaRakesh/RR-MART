a73588f58bc927e9d965b44248e1b6c7bb1fc897
BhimaRakesh <brakeshrakesh5@gmail.com>
2025-01-28 03:47:58 +0530

    do commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base :"/"
   // Update this to match your GitHub repository name
 });
 
