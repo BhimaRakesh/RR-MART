23a389d5518330bae4d004e15cce0dd3e061f048
BhimaRakesh <brakeshrakesh5@gmail.com>
2025-01-29 01:19:23 +0530

    last one

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/RR-MART/',  
+  base: '/RR-MART/',  
   build: {
     outDir: 'docs',  
     assetsDir: 'static',
